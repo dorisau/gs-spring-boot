a3e3d3bd13ac41004a48011b0177c9f421ed3c27
doris <aulala08@gmail.com>
2022-01-05 23:11:37 +0800

    log4j2 with spring-boot

--- complete/src/main/java/com/example/springboot/Application.java
+++ complete/src/main/java/com/example/springboot/Application.java
@@ -2,9 +2,10 @@ package com.example.springboot;
 
 import java.util.Arrays;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +15,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
-//	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-	private static final Logger LOGGER = LogManager.getLogger(Application.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+//	private static final Logger LOGGER = LogManager.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
